6cb4e0ef30fb3157a660e4bd1066a773579ad4a7
J. Gomez <alambike@gmail.com>
2015-01-13 01:37:54 +0100

    Bump to v0.3.4

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "info@riyic.com"
 license          "Apache 2.0"
 description      "Cookbook to deploy ruby applications"
 #long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.3"
+version          "0.3.4"
 
 
 depends "lang_ruby"
